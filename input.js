40b596a99fb8c9f0e6f02d49bfabdca2b2573737
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-21 20:29:32 -0700

    game now takes user inputs and translates them into movements. more comments added and input checks in handleUserInput

--- input.js
+++ input.js
@@ -1,15 +1,37 @@
-const setupInput = function () {
+// stores active TCP connection obj
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  //ctrl + c is the only way to exit the application, now checks to see if what key is and moves snake accordingly
   const handleUserInput = function (key) {
       if (key === '\u0003') {
         process.exit();
       }
+      if (key === "w") {
+        conn.write('Move: up')
+        // console.log('pressed ' + key)
+      }
+      if (key === "a") {
+        conn.write('Move: left')
+        // console.log('pressed ' + key)
+      }
+      if (key === "s") {
+        conn.write('Move: down')
+        // console.log('pressed ' + key)
+      }
+      if (key === "d") {
+        conn.write('Move: right')
+        // console.log('pressed ' + key)
+      }
   }
 
+  // when data is being sent to the server, it calls back handleUserInput
   stdin.on("data", handleUserInput);
 
   return stdin;
--- play.js
+++ play.js
@@ -3,5 +3,5 @@ const { connect } = require("./client")
 const { setupInput } = require('./input')
 
 console.log("Connecting ...");
-connect();
-setupInput();
+//connect();
+setupInput(connect());
